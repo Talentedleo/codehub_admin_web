36ea4dc23876257befe19eb71f357c9f0f48114b
Talentedleo <grandleo@163.com>
2019-05-05 11:00:52 +0800

    修改prod局域网ip

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '"http://127.0.0.1:9011"'
+  BASE_API: '"http://172.18.228.124:9011"'
 }
